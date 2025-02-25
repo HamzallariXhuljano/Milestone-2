6b664ce5e2367432dc23b214780102484464a958
HamzallariXhuljano <xhuljanohamzallari42@gmail.com>
2025-02-25 15:48:49 +0100

    pipex start

---
+++ pipex/source/pipext.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipext.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xhamzall <xhamzall@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/25 15:43:54 by xhamzall          #+#    #+#             */
+/*   Updated: 2025/02/25 15:46:50 by xhamzall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+# include "../printf/ft_printf.h"
+# include "../get_next_line/get_next_line.h"
+
+#endif
